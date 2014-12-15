db6f4f0d787a08b4e6ee8dfe95e70162e8565d1b
Patrick Deelen <patrickdeelen@gmail.com>
2014-12-15 10:55:39 +0100

    qtl annotator

--- QtlAnnotator/src/main/java/nl/systemsgenetics/qtlannotator/QtlAnnotator.java
+++ QtlAnnotator/src/main/java/nl/systemsgenetics/qtlannotator/QtlAnnotator.java
@@ -1,11 +1,11 @@
 package nl.systemsgenetics.qtlannotator;
 
 import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.String;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import org.apache.commons.cli.CommandLine;
@@ -16,6 +16,7 @@ import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
 import org.molgenis.genotype.RandomAccessGenotypeData;
 import org.molgenis.genotype.RandomAccessGenotypeDataReaderFormats;
+import org.molgenis.genotype.util.Ld;
 import org.molgenis.genotype.variant.GeneticVariant;
 import umcg.genetica.collections.ChrPosMap;
 import umcg.genetica.io.trityper.EQTL;
@@ -130,7 +131,7 @@ public class QtlAnnotator {
 			System.exit(1);
 			return;
 		}
-		
+
 		System.out.println("QTL file: " + qtlFile);
 		System.out.println("Annotion file: " + annotationFile);
 		System.out.println("Output file: " + outputFile);
@@ -154,58 +155,119 @@ public class QtlAnnotator {
 			}
 		}
 
-		
-		
-		
+
+
+
 		ChrPosMap<String[]> annotationMap = new ChrPosMap<String[]>();
 		CSVReader reader = new CSVReader(new FileReader(annotationFile));
 		String[] nextLine = reader.readNext();
-		
-		if(nextLine.length < 3){
+
+		if (nextLine.length < 3) {
 			System.out.println("No annotations found in header");
 		}
 		final String[] annotatationNames = new String[nextLine.length - 2];
-		for(int i = 2 ; i < nextLine.length ; ++i){
-			annotatationNames[i-2] = nextLine[i];
+		for (int i = 2; i < nextLine.length; ++i) {
+			annotatationNames[i - 2] = nextLine[i];
 		}
-		
+
 		while ((nextLine = reader.readNext()) != null) {
-			if(nextLine.length != annotatationNames.length + 2){
+			if (nextLine.length != annotatationNames.length + 2) {
 				throw new Exception("Error in annotation different number of columns then headers on line: " + Arrays.toString(nextLine));
 			}
 			annotationMap.put(nextLine[1], Integer.valueOf(nextLine[2]), Arrays.copyOfRange(nextLine, 2, nextLine.length));
 		}
 		reader.close();
-		
+
 		RandomAccessGenotypeData genotypeData = RandomAccessGenotypeDataReaderFormats.valueOf(genotypeDataType.toUpperCase()).createGenotypeData(genotypeDataPaths, 10000);
 
 		QTLTextFile eQTLsTextFile = new QTLTextFile(qtlFile.getAbsolutePath(), false);
+
+		
+
+		CSVWriter mappingReportWriter = new CSVWriter(new FileWriter(outputFile), '\t', CSVWriter.NO_QUOTE_CHARACTER);
+		
+		final String[] qtlAnnotatedOutput = new String[5 + annotatationNames.length];
+		int c = 0;
+		qtlAnnotatedOutput[c++] = "p-value";
+		qtlAnnotatedOutput[c++] = "variant";
+		qtlAnnotatedOutput[c++] = "chr";
+		qtlAnnotatedOutput[c++] = "pos";
+		
+		for(String annotationName : annotatationNames){
+			qtlAnnotatedOutput[c++] = annotationName;
+		}
+		mappingReportWriter.writeNext(qtlAnnotatedOutput);
+		
+		final StringBuilder[] qtlAnnotations = new StringBuilder[annotatationNames.length];
+		
 		qtls:
 		for (Iterator<EQTL> eQtlIt = eQTLsTextFile.getEQtlIterator(); eQtlIt.hasNext();) {
 
 			EQTL eQtl = eQtlIt.next();
 			String chr = eQtl.getRsChr() + "";
 			int pos = eQtl.getRsChrPos();
-			
-			GeneticVariant qtlVariant = genotypeData.getSnpVariantByPos(chr, pos);
-			
-			if(qtlVariant == null){
-				System.err.println("ERROR: qtl variant " + eQtl.getRsName() + " not found in reference. Skipping variant.");
-				continue qtls;
-			}
-			
-			otherVariants:
-			for(GeneticVariant variant : genotypeData.getVariantsByRange(chr, pos - window, pos + window)){
-				
-				if(variant == qtlVariant){
-					continue otherVariants;
+
+			for (int i = 0; i < annotatationNames.length; ++i) {
+				String[] annotation = annotationMap.get(chr, pos);
+				if (annotation == null) {
+					qtlAnnotations[i] = new StringBuilder();
+				} else {
+					qtlAnnotations[i] = new StringBuilder(annotation[i]);
 				}
-				
-				
-				
+
 			}
 
+			GeneticVariant qtlVariant = genotypeData.getSnpVariantByPos(chr, pos);
+
+			if (qtlVariant == null) {
+				System.err.println("ERROR: QTL variant " + eQtl.getRsName() + " not found in reference. Unable to search for variants in LD for this QTL.");
+			} else {
+
+				otherVariants:
+				for (GeneticVariant variant : genotypeData.getVariantsByRange(chr, pos - window, pos + window)) {
+
+					if (variant == qtlVariant) {
+						continue otherVariants;
+					}
+
+					Ld ld = variant.calculateLd(qtlVariant);
+
+					if (ld.getR2() < minR2) {
+						continue otherVariants;
+					}
+
+					String[] annotation = annotationMap.get(variant.getSequenceName(), variant.getStartPos());
+
+					if (annotation != null) {
+						for (int i = 0; i < annotatationNames.length; ++i) {
+							if(qtlAnnotations[i].length() != 0){
+								qtlAnnotations[i].append("; ");
+							}
+							qtlAnnotations[i].append(annotation[i]);
+						}
+					}
+
+				}
+
+			}
+
+			c = 0;
+			qtlAnnotatedOutput[c++] = String.valueOf(eQtl.getPvalue());
+			qtlAnnotatedOutput[c++] = String.valueOf(eQtl.getRsName());
+			qtlAnnotatedOutput[c++] = chr;
+			qtlAnnotatedOutput[c++] = String.valueOf(pos);
+
+			for(StringBuilder annotation : qtlAnnotations){
+				qtlAnnotatedOutput[c++] = annotation.toString();
+			}
+			
+			mappingReportWriter.writeNext(qtlAnnotatedOutput);
+			
 		}
+		
+		mappingReportWriter.close();
+		
+		System.out.println("Annotation done");
 
 	}
 }
